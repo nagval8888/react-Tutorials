3daebfd811a8f06c5921d17337e9841260953aa1
Sergey Korovin <nagval8888@mail.ru>
2018-03-24 20:55:18 +0500

    Calculator - Celsius->Fahrenheit

--- src/index.js
+++ src/index.js
@@ -170,7 +170,7 @@ ReactDOM.render(
 	<ToonList cartoon={cartoons}/>,
 	document.getElementById('root')
 );*/
-/*10. Form,input,checkbox,textarea,select*/
+/*10. Form,input,checkbox,textarea,select
 class FormTest extends Component {
     constructor(props){
         super(props);
@@ -212,4 +212,48 @@ class FormTest extends Component {
 ReactDOM.render(
 	<FormTest />,
 	document.getElementById('root')
-);
+);*/
+/*11. Calculator - Celsius->Fahrenheit
+class Calculator extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            scale: 'c',
+			temp: 0
+        };
+    }
+    handleCelsius = (event)=>{
+    	this.setState({
+			scale: 'c',
+			temp: event.target.value
+    	});
+    };
+	handleFahrenheit = (event)=>{
+		this.setState({
+			scale: 'f',
+			temp: event.target.value
+		});
+	};
+    render(){
+    	const temp = this.state.temp;
+    	const scale = this.state.scale;
+    	const celsius = scale === 'f' ? (temp-32)*5/9 : temp ;
+    	const fahrenheit = scale === 'c' ? (temp*9/5)+32 : temp ;
+        return (
+        	<div>
+				<fieldset>
+					<legend>Scale Celsius</legend>
+					<input value={celsius} onChange={this.handleCelsius}/>
+				</fieldset>
+				<fieldset>
+					<legend>Scale Fahrenheit</legend>
+					<input value={fahrenheit} onChange={this.handleFahrenheit}/>
+				</fieldset>
+			</div>
+        );
+    }
+}
+ReactDOM.render(
+    <Calculator />,
+    document.getElementById('root')
+);*/
